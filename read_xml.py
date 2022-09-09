7f4dd77747a1caeddb19af44297891b9f850c541
Sulaiman Olubiyi <71001536+Damdev-95@users.noreply.github.com>
2022-09-09 19:27:43 +0100

    Update read_xml.py

--- read_xml.py
+++ read_xml.py
@@ -1,4 +1,5 @@
 #In this assignment you will write a Python program somewhat similar to http://www.py4e.com/code3/geoxml.py. The program will prompt for a URL, read the XML data from that URL using urllib and then parse and extract the comment counts from the XML data, compute the sum of the numbers in the file.
+#Don't name the python file xml.py 
 
 
 from urllib import request
