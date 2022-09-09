5c82b322ab95e12f21e5210166a165093b76e8ac
Sulaiman Olubiyi <71001536+Damdev-95@users.noreply.github.com>
2022-09-09 18:52:15 +0100

    Update scrap_number.py

--- scrap_number.py
+++ scrap_number.py
@@ -1,4 +1,5 @@
 #Scraping Numbers from HTML using BeautifulSoup In this assignment you will write a Python program similar to http://www.py4e.com/code3/urllink2.py. The program will use urllib to read the HTML from the data files below, and parse the data, extracting numbers and compute the sum of the numbers in the file.
+#using ciscoIDE
 
 from urllib import request
 from bs4 import BeautifulSoup
