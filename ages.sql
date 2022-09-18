dd93c3f57f3df11c3107514c05218b7dd1b2a8d4
Sulaiman Olubiyi <71001536+Damdev-95@users.noreply.github.com>
2022-09-18 12:03:45 +0100

    Create ages.sql

---
+++ ages.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Ages ( 
+  name VARCHAR(128), 
+  age INTEGER
+)
+
+DELETE FROM Ages;
+INSERT INTO Ages (name, age) VALUES ('Aurlah', 27);
+INSERT INTO Ages (name, age) VALUES ('Pascoe', 25);
+INSERT INTO Ages (name, age) VALUES ('Sohaib', 36);
+INSERT INTO Ages (name, age) VALUES ('Arda', 28);
+INSERT INTO Ages (name, age) VALUES ('Rooke', 17);
+INSERT INTO Ages (name, age) VALUES ('Circe', 25);
+
